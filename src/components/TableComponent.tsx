a27624b3feaa88ea09b76e9c057e4672af0c9696
Jonis Bergman <jonis.bergman@leovegas.com>
2023-03-30 21:59:56 +0200

    added some testobjects for mapping out in table

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -26,10 +26,47 @@ type Props = {};
 const TableComponent = (props: Props) => {
   const [show, setShow] = useState(false);
   const [searchResultState, setSearchResultState] = useState({});
-  const poster = "/5VTN0pR8gcqV3EPUHHfMGnJYN9L.jpg";
 
   const handleShow = () => {
     setShow(true);
+  };
+
+  const testMovie = {
+    0: {
+      adult: false,
+      backdrop_path: "/7ABgKch4hBX4qwCujRKM58fKsoG.jpg",
+      genre_ids: [12, 14, 28],
+      id: 121,
+      original_language: "en",
+      original_title: "The Lord of the Rings: The Two Towers",
+      overview:
+        "Frodo and Sam are trekking to Mordor to destroy the One Ring of Power while Gimli, Legolas and Aragorn search for the orc-captured Merry and Pippin. All along, nefarious wizard Saruman awaits the Fellowship members at the Orthanc Tower in Isengard.",
+      popularity: 82.443,
+      poster_path: "/5VTN0pR8gcqV3EPUHHfMGnJYN9L.jpg",
+      release_date: "2002-12-18",
+      title: "The Lord of the Rings: The Two Towers",
+      video: false,
+      vote_average: 8.4,
+      vote_count: 19510,
+    },
+
+    1: {
+      adult: false,
+      backdrop_path: "/ysKahAEPP8h6MInuLjr0xuZOTjh.jpg",
+      genre_ids: [80, 35],
+      id: 107,
+      original_language: "en",
+      original_title: "Snatch",
+      overview:
+        "Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers and supposedly Jewish jewelers fight to track down a priceless stolen diamond.",
+      popularity: 28.843,
+      poster_path: "/56mOJth6DJ6JhgoE2jtpilVqJO.jpg",
+      release_date: "2000-09-01",
+      title: "Snatch",
+      video: false,
+      vote_average: 7.8,
+      vote_count: 7924,
+    },
   };
 
   return (
@@ -50,18 +87,25 @@ const TableComponent = (props: Props) => {
             <td style={{ maxHeight: "3rem", maxWidth: "3rem" }}>
               <img
                 alt="poster"
-                src={"https://image.tmdb.org/t/p/w200" + poster}
+                src={
+                  "https://image.tmdb.org/t/p/w200" + testMovie[0].poster_path
+                }
                 style={{ maxHeight: "100%", maxWidth: "100%" }}
               />
             </td>
             <td>
-              <p style={{ padding: "4px 8px", marginTop: "1.9rem" }}>
-                The lord of the rings: The two towers
-              </p>
+              <div style={{ padding: "4px 8px", marginTop: "1.9rem" }}>
+                <p>
+                  {testMovie[0].title}
+                  <small style={{ color: "GrayText" }}>
+                    {" (" + testMovie[0].release_date + ")"}
+                  </small>
+                </p>
+              </div>
             </td>
             <td>
               <div style={{ padding: "4px 8px", marginTop: "1.9rem" }}>
-                9.7&nbsp;
+                {testMovie[0].vote_average}&nbsp;
                 <FontAwesomeIcon color="gold" icon={faStar} />
               </div>
             </td>
