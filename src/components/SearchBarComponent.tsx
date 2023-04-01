aa3b378b9abbdcc325c1645f34edecfb8b2c4246
Jonis Bergman <jonis.bergman@leovegas.com>
2023-04-01 16:19:50 +0200

    moved logic out of searchbar

--- src/components/SearchBarComponent.tsx
+++ src/components/SearchBarComponent.tsx
@@ -1,99 +1,42 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-import Form from "react-bootstrap/Form";
 import FormGroup from "react-bootstrap/FormGroup";
 import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
+import FormText from "react-bootstrap/FormText";
 
 import Container from "react-bootstrap/Container";
 
-import useDebounce from "../hooks/useDebounce";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { FormText } from "react-bootstrap";
-
-type Props = {};
-
-const SearchBar = (props: Props) => {
-  const [initialList, setInitialList] = useState([]);
-  const [searchResult, setSearchResult] = useState([]);
-  const [hideSearchDisplay, setHideSearchDisplay] = useState(true);
-
-  const [searchQuery, setSearchQuery] = useState("");
-  const debouncedValue = useDebounce<string>(searchQuery, 500);
-
-  const TMDB_KEY = "47da03161ab5477b6ac6c70f5435f4ae";
-
-  // welcome list
-  useEffect(() => {
-    const initData = async () => {
-      try {
-        await axios
-          .get(
-            `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_KEY}&language=en-US&page=1`
-          )
-          .then((res) => {
-            setInitialList(res.data.results);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    initData();
-  }, []);
-
-  //search
-  useEffect(() => {
-    if (debouncedValue !== "") {
-      try {
-        axios
-          .get(
-            `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=${debouncedValue}&page=1&include_adult=false`
-          )
-          .then((res) => {
-            setSearchResult(res.data.results);
-            setHideSearchDisplay(false);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }, [debouncedValue]);
-
-  console.log(searchResult);
-
-  function handleChange(
+type SearchBarProps = {
+  searchQuery?: string;
+  hideSearchDisplay: boolean;
+  handleChange(
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ): void {
-    if (event.target.value !== undefined) {
-      setSearchQuery(event?.target?.value);
-    }
-  }
+  ): void;
+};
+
+const SearchBar = (props: SearchBarProps) => {
+  const { searchQuery, handleChange, hideSearchDisplay } = props;
 
   return (
     <>
-      <Form>
-        <Container>
-          <FormGroup className="d-flex flex-column justify-content-center">
-            <FormControl
-              type="text"
-              placeholder="Search for your favorite movies!"
-              value={searchQuery}
-              onChange={handleChange}
-              style={{ marginTop: "4rem" }}
-            />
-            <FormText
-              className="mx-auto"
-              hidden={hideSearchDisplay}
-              style={{ color: "#FFFFF" }}
-            >
-              Displaying results for search : {searchQuery}
-            </FormText>
-          </FormGroup>
-        </Container>
-      </Form>
+      <Container>
+        <FormGroup className="d-flex flex-column justify-content-center">
+          <FormControl
+            type="text"
+            placeholder="Search for your favorite movies!"
+            value={searchQuery}
+            onChange={handleChange}
+            style={{ marginTop: "4rem" }}
+          />
+          <FormText
+            className="mx-auto"
+            hidden={hideSearchDisplay}
+            style={{ color: "#FFFFF" }}
+          >
+            Displaying results for search : {searchQuery}
+          </FormText>
+        </FormGroup>
+      </Container>
     </>
   );
 };
--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import useDebounce from "../hooks/useDebounce";
+import axios from "axios";
 
 import Table from "react-bootstrap/Table";
 
@@ -69,9 +71,70 @@ const TableComponent = (props: Props) => {
     },
   };
 
+  const [initialList, setInitialList] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+  const [hideSearchDisplay, setHideSearchDisplay] = useState(true);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const debouncedValue = useDebounce<string>(searchQuery, 500);
+
+  const TMDB_KEY = "47da03161ab5477b6ac6c70f5435f4ae";
+
+  // welcome list
+  useEffect(() => {
+    const initData = async () => {
+      try {
+        await axios
+          .get(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_KEY}&language=en-US&page=1`
+          )
+          .then((res) => {
+            setInitialList(res.data.results);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    initData();
+  }, []);
+
+  //search
+  useEffect(() => {
+    if (debouncedValue !== "") {
+      try {
+        axios
+          .get(
+            `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=${debouncedValue}&page=1&include_adult=false`
+          )
+          .then((res) => {
+            setSearchResult(res.data.results);
+            setHideSearchDisplay(false);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [debouncedValue]);
+
+  console.log(searchResult);
+
+  function handleChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void {
+    event.preventDefault();
+
+    if (event.target.value !== undefined) {
+      setSearchQuery(event?.target?.value);
+    }
+  }
+
   return (
     <>
-      <SearchBar />
+      <SearchBar
+        searchQuery={searchQuery}
+        hideSearchDisplay={hideSearchDisplay}
+        handleChange={handleChange}
+      />
       <Container>
         <Table striped hover variant="dark" style={{ marginTop: "2rem" }}>
           <thead>
