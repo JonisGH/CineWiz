98e47283a8ed3d377592f7356cfaa618f343193e
Jonis Bergman <jonis021@gmail.com>
2023-04-08 18:03:59 +0200

    fixed tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,20 +1,64 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
-import App from '../App';
+//Components used in tests
+import App from "../App";
+import Table from "react-bootstrap/Table";
+import TableRowComponent from "../components/TableRowComponent";
+import NavbarComponent from "../components/NavbarComponent";
+// Types used in tests
+import { MovieObject } from "../context/appContext";
 
-describe('App', () => {
-  it('renders without crashing', () => {
-    render(<App />);
-    const appElement = screen.getByTestId('app');
-    expect(appElement).toBeInTheDocument();
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), 
+    removeListener: jest.fn(), 
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+const mockMovie: MovieObject = {
+  title: "The Mockening Movie",
+  release_date: "2020-20-20",
+  poster_path: "/mockURLmOcKuRl",
+  vote_average: 5.5,
+  id: 1337,
+  overview:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio necessitatibus ducimus nobis fugiat accusamus voluptatem doloribus, voluptatum eum? Esse, molestias?",
+};
+
+const mockResult = [mockMovie, mockMovie, mockMovie, mockMovie];
+
+describe("App", () => {
+  it("renders without crashing", () => {
+    expect(render(<App />)).toBeTruthy();
   });
+});
 
-  // it('opens offcanvas when "My Stuff" button is clicked', () => {
-  //   render(<App />);
-  //   const myStuffButton = screen.getByRole('button', { name: 'My Stuff' });
-  //   fireEvent.click(myStuffButton);
-  //   const offCanvasElement = screen.getByTestId('offcanvas');
-  //   expect(offCanvasElement).toBeInTheDocument();
-  // });
+test("able to open sidebar onclick", () => {
+  render(<NavbarComponent />);
+
+  //click button
+  fireEvent.click(screen.getByRole("button"));
+  //expect sidebar to appear
+  expect(screen.getByRole("dialog")).toBeTruthy();
+});
+
+test("tablerowcomponents maps to table and renders movies", () => {
+  render(
+    <Table>
+      <tbody>
+        {mockResult.map((movie, idx) => {
+          return <TableRowComponent key={idx} movie={movie} />;
+        })}
+      </tbody>
+    </Table>
+  );
+  expect(screen.getAllByText("The Mockening Movie")).toHaveLength(4);
 });
--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import NavbarBrand from 'react-bootstrap/NavbarBrand';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+import Navbar from "react-bootstrap/Navbar";
 
-import SideBarComponent from './SideBarComponent';
+import SideBarComponent from "./SideBarComponent";
 
-import Button from 'react-bootstrap/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilm } from '@fortawesome/free-solid-svg-icons';
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const NavbarComponent = (): JSX.Element => {
   const [sidebarOpen, setSiderbarOpen] = useState(false);
@@ -22,16 +22,17 @@ const NavbarComponent = (): JSX.Element => {
     <>
       <Navbar fixed="top" bg="dark" variant="dark">
         <Container
-          style={{ transition: 'margin-right .5s', paddingRight: '20px' }}
+          style={{ transition: "margin-right .5s", paddingRight: "20px" }}
         >
           <NavbarBrand>
             <FontAwesomeIcon icon={faFilm} /> CineWiz
           </NavbarBrand>
           <Button
+            role="button"
             variant="outline-light"
             style={{
-              border: 'none',
-              fontWeight: 'bold',
+              border: "none",
+              fontWeight: "bold",
             }}
             onClick={toggleSidebarOpen}
           >
